3df4e88d5464052bd355e014830d6e9365c474da
Donghyeok Lim <jh07050@gmail.com>
2025-07-19 17:23:43 +0900

    remove: 미사용 초기 User Dto 제거

--- api/src/main/kotlin/com/benecia/lifetracker/domain/user/dto/NewUserRequest.kt
+++
@@ -1,5 +0,0 @@
-package com.benecia.lifetracker.domain.user.dto
-
-data class NewUserRequest(
-    val name: String,
-)
--- api/src/main/kotlin/com/benecia/lifetracker/domain/user/dto/NewUserResponse.kt
+++
@@ -1,5 +0,0 @@
-package com.benecia.lifetracker.domain.user.dto
-
-data class NewUserResponse(
-    val id: Long
-)
