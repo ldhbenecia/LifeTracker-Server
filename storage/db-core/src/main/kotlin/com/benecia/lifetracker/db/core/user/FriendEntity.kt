0a8e6f714f1bedb12ce45f3231add5a079a39b71
Donghyeok Lim <jh07050@gmail.com>
2025-07-22 18:29:30 +0900

    feat: FriendEntity에 @GeneratedValue 적용

--- storage/db-core/src/main/kotlin/com/benecia/lifetracker/db/core/user/FriendEntity.kt
+++ storage/db-core/src/main/kotlin/com/benecia/lifetracker/db/core/user/FriendEntity.kt
@@ -7,6 +7,7 @@ import jakarta.persistence.Entity
 import jakarta.persistence.EnumType
 import jakarta.persistence.Enumerated
 import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.Table
 import jakarta.persistence.UniqueConstraint
@@ -22,7 +23,7 @@ import java.util.UUID
 class FriendEntity(
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
 
     @Column(nullable = false)
