19c180599075ac9a0a7a83960eb4d203b60118a3
Donghyeok Lim <jh07050@gmail.com>
2025-07-25 17:05:32 +0900

    feat: Health Controller 작성

---
+++ api/src/main/kotlin/com/benecia/lifetracker/health/HealthController.kt
@@ -0,0 +1,12 @@
+package com.benecia.lifetracker.health
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HealthController {
+    @GetMapping("/health")
+    fun health(): String {
+        return "OK"
+    }
+}
