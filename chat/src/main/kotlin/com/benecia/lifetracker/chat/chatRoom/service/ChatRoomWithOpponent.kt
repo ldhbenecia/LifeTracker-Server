8737f5deac04cc90bdc0487ed24e6cde4515f733
Donghyeok Lim <jh07050@gmail.com>
2025-08-01 21:33:07 +0900

    feat: 채팅방과 상대 정보 Dto

---
+++ chat/src/main/kotlin/com/benecia/lifetracker/chat/chatRoom/service/ChatRoomWithOpponent.kt
@@ -0,0 +1,13 @@
+package com.benecia.lifetracker.chat.chatRoom.service
+
+import java.time.LocalDateTime
+import java.util.UUID
+
+data class ChatRoomWithOpponent(
+    val roomId: Long,
+    val lastMessage: String?,
+    val lastMessageTime: LocalDateTime?,
+    val opponentUserId: UUID,
+    val opponentUserName: String,
+    val opponentProfileImageUrl: String?
+)
