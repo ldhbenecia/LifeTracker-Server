94d896646107185cacd6a132e8f5d96d815c4017
Donghyeok Lim <jh07050@gmail.com>
2025-08-01 20:36:35 +0900

    feat: ChatRoomUser Entity 생성
    - ChatRoom, User 중간 테이블

---
+++ storage/db-core-jpa/src/main/kotlin/com/benecia/lifetracker/db/jpa/chat/ChatRoomUserEntity.kt
@@ -0,0 +1,34 @@
+package com.benecia.lifetracker.db.jpa.chat
+
+import jakarta.persistence.Column
+import jakarta.persistence.Embeddable
+import jakarta.persistence.Embedded
+import jakarta.persistence.EmbeddedId
+import jakarta.persistence.Entity
+import jakarta.persistence.Table
+import java.io.Serializable
+import java.time.LocalDateTime
+import java.util.UUID
+
+@Entity
+@Table(name = "chat_room_user")
+class ChatRoomUserEntity(
+
+    @EmbeddedId
+    val id: ChatRoomUserId,
+
+    val joinedAt: LocalDateTime = LocalDateTime.now(),
+
+    @Column(name = "notification_enabled")
+    var notificationEnabled: Boolean = true
+)
+
+@Embeddable
+class ChatRoomUserId(
+
+    @Column(name = "room_id")
+    val roomId: Long,
+
+    @Column(name = "user_id", columnDefinition = "BINARY(16)")
+    val userId: UUID
+) : Serializable
