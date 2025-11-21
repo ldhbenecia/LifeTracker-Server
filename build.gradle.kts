0a3b3fc6d221fe4de18e2c60a8071c3a063dbaff
Donghyeok Lim <jh07050@gmail.com>
2025-11-21 17:05:42 +0900

    style: ktlint formatting

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,66 +1,66 @@
 plugins {
-	kotlin("jvm")
-	kotlin("kapt")
-	kotlin("plugin.spring") apply false
-	kotlin("plugin.jpa") apply false
-	id("org.springframework.boot") apply false
-	id("io.spring.dependency-management") apply false
-	id("org.asciidoctor.jvm.convert") apply false
-	id("org.jlleitschuh.gradle.ktlint") apply false
+    kotlin("jvm")
+    kotlin("kapt")
+    kotlin("plugin.spring") apply false
+    kotlin("plugin.jpa") apply false
+    id("org.springframework.boot") apply false
+    id("io.spring.dependency-management") apply false
+    id("org.asciidoctor.jvm.convert") apply false
+    id("org.jlleitschuh.gradle.ktlint") apply false
 }
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of("${property("javaVersion")}")
-	}
+    toolchain {
+        languageVersion = JavaLanguageVersion.of("${property("javaVersion")}")
+    }
 }
 
 allprojects {
-	group = "${property("projectGroup")}"
-	version = "${property("applicationVersion")}"
+    group = "${property("projectGroup")}"
+    version = "${property("applicationVersion")}"
 
-	repositories {
-		mavenCentral()
-	}
+    repositories {
+        mavenCentral()
+    }
 }
 
 subprojects {
-	apply(plugin = "org.jetbrains.kotlin.jvm")
-	apply(plugin = "org.jetbrains.kotlin.kapt")
-	apply(plugin = "org.jetbrains.kotlin.plugin.spring")
-	apply(plugin = "org.jetbrains.kotlin.plugin.jpa")
-	apply(plugin = "org.springframework.boot")
-	apply(plugin = "io.spring.dependency-management")
-	apply(plugin = "org.asciidoctor.jvm.convert")
-	apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "org.jetbrains.kotlin.kapt")
+    apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+    apply(plugin = "org.jetbrains.kotlin.plugin.jpa")
+    apply(plugin = "org.springframework.boot")
+    apply(plugin = "io.spring.dependency-management")
+    apply(plugin = "org.asciidoctor.jvm.convert")
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
 
-	dependencies {
-		implementation("org.jetbrains.kotlin:kotlin-reflect")
-		implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-		implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-		testImplementation("org.springframework.boot:spring-boot-starter-test")
+    dependencies {
+        implementation("org.jetbrains.kotlin:kotlin-reflect")
+        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
         testImplementation("com.ninja-squad:springmockk:${property("springMockkVersion")}")
-		annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-		kapt("org.springframework.boot:spring-boot-configuration-processor")
-	}
+        annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+        kapt("org.springframework.boot:spring-boot-configuration-processor")
+    }
 
-	tasks.getByName("bootJar") {
-		enabled = false
-	}
+    tasks.getByName("bootJar") {
+        enabled = false
+    }
 
-	tasks.getByName("jar") {
-		enabled = true
-	}
+    tasks.getByName("jar") {
+        enabled = true
+    }
 
-	kotlin {
-		compilerOptions {
-			freeCompilerArgs.addAll("-Xjsr305=strict")
-		}
-	}
+    kotlin {
+        compilerOptions {
+            freeCompilerArgs.addAll("-Xjsr305=strict")
+        }
+    }
 
-	tasks.withType<Test> {
-		useJUnitPlatform()
-	}
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
 
     tasks.test {
         useJUnitPlatform {
