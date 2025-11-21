edda49233c285d7df1ae472dc340dccfab8d0031
Donghyeok Lim <jh07050@gmail.com>
2025-11-21 17:35:18 +0900

    chore: Chat Module spring-data-redis 의존성 부여

--- chat/build.gradle.kts
+++ chat/build.gradle.kts
@@ -6,4 +6,5 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-websocket")
     implementation("org.springframework.boot:spring-boot-starter-amqp")
     implementation("org.springframework.security:spring-security-messaging")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
 }
